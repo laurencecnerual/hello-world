1c8553023c2e4fc25d988a93781af7d8ffe4827d
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2019-02-19 13:17:53 +0900

    Significant Changes to Main
    Comments and error checking/messages were added, some variable names and input values were also changed, and most notably, some of the code was moved to a new Class that Main now invokes (see Calculator).

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+//This program allows users to input two numbers, and perform the mathematical operation of their choosing between them
+//(ie. addition, subtraction, multiplication, or division)
+
 package com.company;
 import java.util.Scanner;
 
@@ -5,37 +8,52 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner getInput = new Scanner(System.in);
-        double a; // the user's first numerical input
-        double b; // the user's second numerical input
-        double c = 0; // the sum, difference, product, or quotient of the two numbers supplied by the user
-        char mathType = 'F'; // the user's desired mathematical operation type
+        double arg1 = 0; // the user's first numerical input
+        double arg2 = 0; // the user's second numerical input
+        double answer; // the sum, difference, product, or quotient of the two numbers supplied by the user
+        char mathOp; // the user's desired mathematical operation type
+        Calculator myCalc = new Calculator();
 
-        do {
-            System.out.println("What type of calculation would you like to make?");
-            System.out.println("Enter A for Addition, S for Subtraction, M for Multiplication, or D for Division.");
-            mathType = (getInput.next()).charAt(0);
-            System.out.println();
-        } while ((mathType != 'A') && (mathType != 'S') && (mathType != 'M') && (mathType != 'D'));
+        System.out.println("Hello! What type of calculation would you like to make?");
+        System.out.println("Enter + for Addition, - for Subtraction, * for Multiplication, or / for Division.");
 
-        System.out.println("Enter any two numbers, separated by either a new line or a space.");
-        a = getInput.nextDouble();
-        b = getInput.nextDouble();
-
-        switch (mathType) {
-            case 'A':
-                c = a + b;
+        while (true) {
+            mathOp = (getInput.next()).charAt(0);
+            if ((mathOp != '+') && (mathOp != '-') && (mathOp != '*') && (mathOp != '/')) {
+                System.out.println("Uh oh. It seems you did not make a valid selection. Please try again.");
+            }
+            else {
+                System.out.println("Thank you.");
+                System.out.println();
                 break;
-            case 'S':
-                c = a - b;
-                break;
-            case 'M':
-                c = a * b;
-                break;
-            case 'D':
-                c = a / b;
-                break;
+            }
         }
 
-        System.out.println("The result of the operation you selected is " + c + ".");
+        System.out.println("Enter the two numbers you would like to perform the operation on, separated by either a new line or a space.");
+
+        while (!getInput.hasNextDouble()) {
+            getInput.next();
+            System.out.println("Uh oh. It seems you did not enter a number. Please try again.");
+        }
+
+        arg1 = getInput.nextDouble();
+
+        while (!getInput.hasNextDouble()) {
+            getInput.next();
+            System.out.println("Uh oh. It seems you did not enter a number. Please try again.");
+        }
+
+        arg2 = getInput.nextDouble();
+        System.out.println("Thank you.");
+        System.out.println();
+
+        myCalc.setArgs(arg1, arg2, mathOp);
+        answer = myCalc.getResult();
+
+        System.out.println("According to our calculations...");
+        System.out.println(arg1 + " " + mathOp + " " + arg2 + " = " + answer + ".");
+        System.out.println();
+        System.out.println("We hope you have enjoyed using our services.");
+        System.out.println("Thank you, and have a nice day.");
     }
 }
